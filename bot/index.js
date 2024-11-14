5ce51d9926944a46ffe5455a414ed35f3ceef967
mr.musaozkan <mr.musaozkan@gmail.com>
2024-11-14 20:46:10 +0300

    Updated relative directories

--- bot/index.js
+++ bot/index.js
@@ -1,11 +1,15 @@
-const sendMessages = require('./src/sendMessages'); // Adjusted path
-const settings = require('./config/settings.json'); // Adjusted if config is in config folder
-const initializeClient = require('./src/client'); // Adjust the path if client.js is elsewhere
+const path = require('path');
+const sendMessages = require(path.join(__dirname, './src/sendMessages'));
+const settings = require(path.join(__dirname, './config/settings.json'));
+const initializeClient = require(path.join(__dirname, './src/client'));
+const { outputPageStructure } = require(path.join(__dirname, './src/screenshotHandler'));
 
+// Start the dynamic sessions
 async function startDynamicSessions() {
     const accountPromises = settings.accounts.map((accountConfig) =>
         new Promise((resolve) => {
             initializeClient(accountConfig, async (client) => {
+                await outputPageStructure(client); // Optional for debugging purposes
                 await sendMessages(client, accountConfig);
                 resolve();
             });
