8ebd35a7dc1e9bf004c9981b79ff65d44a9de0be
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-11-16 00:14:24 +0300

    Fix import path for Home component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home";
 import AuthLayout from "./components/AuthLayout/AuthLayout";
 import "./App.css";
 
