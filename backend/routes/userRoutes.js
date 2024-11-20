6a8753a17b274b2f973e5f51ce9303387cf78b3a
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-11-21 00:17:52 +0300

    Add session check route to verify user session status

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  checkSession,
   registerUser,
   loginUser,
   logoutUser,
@@ -8,6 +9,7 @@ import {
 const router = express.Router();
 
 // Kullanıcı rotaları
+router.get("/check-session", checkSession); // Check Session
 router.post("/register", registerUser); // Register
 router.post("/login", loginUser); // Login
 router.delete("/logout", logoutUser); // Logout
