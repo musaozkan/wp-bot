5fb54f1f3c51fc3f0e220cbf1ddbfe7b30aa5995
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-11-21 00:17:34 +0300

    Refactor session middleware configuration for clarity

--- backend/middleware/sessionMiddleware.js
+++ backend/middleware/sessionMiddleware.js
@@ -8,17 +8,16 @@ const FileStoreSession = FileStore(session);
 
 const sessionMiddleware = session({
   store: new FileStoreSession({
-    path: "./sessions", // Session dosyalarının kaydedileceği dizin
-    retries: 1, // Kaydetme deneme sayısı
-    ttl: 7200, // Session süresi (saniye cinsinden, burada 2 saat)
+    path: "./sessions",
+    retries: 1,
+    ttl: 7200,
   }),
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 1000 * 60 * 60 * 2, // 2 saat
+    maxAge: 1000 * 60 * 60 * 2,
   },
 });
 
