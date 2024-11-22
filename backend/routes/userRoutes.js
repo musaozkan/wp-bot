0d8767c13b55f8cee61fc1b3c92617244a3737b0
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-11-23 00:00:46 +0300

    Reorder user routes and add authentication protection to logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,17 @@
 import express from "express";
 import {
-  checkSession,
   registerUser,
   loginUser,
   logoutUser,
+  checkSession,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Kullanıcı rotaları
-router.get("/check-session", checkSession); // Check Session
-router.post("/register", registerUser); // Register
-router.post("/login", loginUser); // Login
-router.delete("/logout", logoutUser); // Logout
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.delete("/logout", protect, logoutUser);
+router.get("/check-session", checkSession);
 
 export default router;
