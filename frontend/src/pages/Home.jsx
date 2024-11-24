6c1af7f223368d3607f39bfd1cfee1e3dd231ef8
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-11-25 01:17:12 +0300

    Add NumberListManager component to Home page for enhanced contact list management

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import TemplateManager from "../components/TemplateManager/TemplateManager";
 import SessionManager from "../components/SessionManager/SessionManager";
+import NumberListManager from "../components/NumberListManager/NumberListManager";
 
 function Home() {
   return (
@@ -12,6 +13,11 @@ function Home() {
         </div>
         <div className="col-lg-6">
           <SessionManager />
+        </div>
+      </div>
+      <div className="row gx-4 gy-4">
+        <div className="col-lg-6">
+          <NumberListManager />
         </div>
       </div>
     </div>
