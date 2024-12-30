3e8d90237b76645a7b6861bffab85d61556e6fd2
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-12-30 22:37:06 +0300

    Update BaseService to use dynamic server URL for API requests

--- frontend/src/services/BaseService.jsx
+++ frontend/src/services/BaseService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios örneği oluştur
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: serverUrl,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
